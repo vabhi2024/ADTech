caf330630f85710324c8e71159fca3b465924a2c
vabhi2024 <vabhi2024@gmail.com>
2026-05-16 23:31:57 +0530

    Update form.js

--- form.js
+++ form.js
@@ -4,9 +4,6 @@ document.getElementById("contactForm").addEventListener("submit", async function
     const form = e.target;
     const formData = new FormData(form);
 
-    // ✅ Access key added in JS
-    formData.append("access_key", "fdff9149-19fc-4bf3-8a40-f83b405de0d0");
-
     try {
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
